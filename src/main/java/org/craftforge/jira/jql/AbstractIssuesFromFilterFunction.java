21e3877be71a71d9fadc9e18aac40df43e15f56c
pbojko <pbojko@ac3d3970-c854-40dc-b022-f14d08d717b8>
2010-12-20 23:30:50 +0000

    CFJIRAJQLFP-5, CFJIRAJQLFP-4, CFJIRAJQLFP-3
    git-svn-id: http://svn.craftforge.org/jira/craftforge-jql-functions-plugin/trunk@97 ac3d3970-c854-40dc-b022-f14d08d717b8

--- src/main/java/org/craftforge/jira/jql/AbstractIssuesFromFilterFunction.java
+++ src/main/java/org/craftforge/jira/jql/AbstractIssuesFromFilterFunction.java
@@ -14,12 +14,12 @@
  * limitations under the License.
  * under the License.
  */
-
 package org.craftforge.jira.jql;
 
 import com.atlassian.jira.ComponentManager;
 import com.atlassian.jira.JiraDataType;
 import com.atlassian.jira.JiraDataTypes;
+import com.atlassian.jira.bc.JiraServiceContextImpl;
 import com.atlassian.jira.bc.filter.SearchRequestService;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.search.SearchException;
@@ -29,6 +29,10 @@ import com.atlassian.jira.jql.operand.QueryLiteral;
 import com.atlassian.jira.jql.query.QueryCreationContext;
 import com.atlassian.jira.plugin.jql.function.AbstractJqlFunction;
 import com.atlassian.jira.plugin.jql.function.JqlFunctionModuleDescriptor;
+import com.atlassian.jira.sharing.search.SharedEntitySearchParameters;
+import com.atlassian.jira.sharing.search.SharedEntitySearchParametersBuilder;
+import com.atlassian.jira.sharing.search.SharedEntitySearchResult;
+import com.atlassian.jira.util.Consumer;
 import com.atlassian.jira.util.I18nHelper;
 import com.atlassian.jira.util.MessageSet;
 import com.atlassian.jira.util.MessageSetImpl;
@@ -46,71 +50,105 @@ import java.util.List;
  */
 public abstract class AbstractIssuesFromFilterFunction extends AbstractJqlFunction {
 
-	protected SearchRequestService searchRequestService;
-	protected SearchProvider searchProvider;
-	protected I18nHelper i18nHelper;
+    protected SearchRequestService searchRequestService;
+    protected SearchProvider searchProvider;
+    protected I18nHelper i18nHelper;
 
-	public JiraDataType getDataType() {
-		return JiraDataTypes.ISSUE;
-	}
+    public JiraDataType getDataType() {
+        return JiraDataTypes.ISSUE;
+    }
 
-	@Override
-	public void init(JqlFunctionModuleDescriptor moduleDescriptor) {
-		super.init(moduleDescriptor);
-		ComponentManager componentManager = ComponentManager.getInstance();
-		searchRequestService = componentManager.getSearchRequestService();
-		searchProvider = componentManager.getSearchProvider();
-		i18nHelper = moduleDescriptor.getI18nBean();
-	}
+    @Override
+    public void init(JqlFunctionModuleDescriptor moduleDescriptor) {
+        super.init(moduleDescriptor);
+        ComponentManager componentManager = ComponentManager.getInstance();
+        searchRequestService = componentManager.getSearchRequestService();
+        searchProvider = componentManager.getSearchProvider();
+        i18nHelper = moduleDescriptor.getI18nBean();
+    }
 
-	@Override
-	public boolean isList() {
-		return true;
-	}
+    @Override
+    public boolean isList() {
+        return true;
+    }
 
-	public MessageSet validate(User user, FunctionOperand operand, TerminalClause tc) {
-		MessageSet messages = new MessageSetImpl();
-		final List<String> args = operand.getArgs();
-		if (args.isEmpty()) {
-			messages.addErrorMessage(i18nHelper.getText("abstract-issues-from-filter.bad.num.arguments",operand.getName()));
-		}
-		if (savedFilterIsInvalid(user, args.get(0))) {
-			messages.addErrorMessage(i18nHelper.getText("abstract-issues-from-filter.bad.saved.filter",args.get(0)));
-		}
-		return messages;
-	}
+    public MessageSet validate(User user, FunctionOperand operand, TerminalClause tc) {
+        MessageSet messages = new MessageSetImpl();
+        final List<String> args = operand.getArgs();
+        if (args.isEmpty()) {
+            messages.addErrorMessage(i18nHelper.getText("abstract-issues-from-filter.bad.num.arguments", operand.getName()));
+        }
+        if (savedFilterIsInvalid(user, args.get(0))) {
+            messages.addErrorMessage(i18nHelper.getText("abstract-issues-from-filter.bad.saved.filter", args.get(0)));
+        }
+        return messages;
+    }
 
-	private SearchRequest fetchFilter(final User user, final String filter) {
-		Collection<SearchRequest> filters = searchRequestService.getNonPrivateFilters(user);
-        filters.addAll(searchRequestService.getOwnedFilters(user)); //private
-		for (SearchRequest request : filters) {
-			if (request.getName().equals(filter) || request.getId().toString().equals(filter)) {
-				return request;
-			}
-		}
-		return null;
-	}
+    private SearchRequest fetchFilter(final User user, final String filter) {
+        SearchRequest result = tryToFetchById(user,filter);
+        if (result != null)
+            return result;
+        return tryToFetchByName(user, filter);
+    }
 
-	protected List<Issue> fetchIssuesFromSubfilter(QueryCreationContext qcc, FunctionOperand fo) {
-		try {
-			SearchRequest request = fetchFilter(qcc.getUser(), fo.getArgs().get(0));
-			List<Issue> issues = searchProvider.search(request.getQuery(), qcc.getUser(), new PagerFilter()).getIssues();
-			return issues;
-		} catch (SearchException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
+    private SearchRequest tryToFetchByName(final User user, final String filter) throws IllegalArgumentException {
+        SharedEntitySearchResult<SearchRequest> searchResult = searchRequestService.search(new JiraServiceContextImpl(user), prepareSharedEntirySearchParameyer(filter), 0, 100);
+        SearchRequestByNameConsumer consumer = new SearchRequestByNameConsumer(filter);
+        searchResult.foreach(consumer);
+        return consumer.getFound();
+    }
 
-	protected boolean savedFilterIsInvalid(final User user, final String filter) {
-		return fetchFilter(user, filter) == null;
-	}
+    protected List<Issue> fetchIssuesFromSubfilter(QueryCreationContext qcc, FunctionOperand fo) {
+        try {
+            SearchRequest request = fetchFilter(qcc.getUser(), fo.getArgs().get(0));
+            List<Issue> issues = searchProvider.search(request.getQuery(), qcc.getUser(), new PagerFilter()).getIssues();
+            return issues;
+        } catch (SearchException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
-	protected List<QueryLiteral> convertToQueryLiteraCollection(final FunctionOperand operand, 
-			final Collection<Issue> issues) {
-		List<QueryLiteral> result = new ArrayList<QueryLiteral>(issues.size());
-		for (Issue issue : issues) {
-			result.add(new QueryLiteral(operand, issue.getKey()));
-		}
-		return result;
-	}
+    protected boolean savedFilterIsInvalid(final User user, final String filter) {
+        return fetchFilter(user, filter) == null;
+    }
+
+    protected List<QueryLiteral> convertToQueryLiteraCollection(final FunctionOperand operand,
+            final Collection<Issue> issues) {
+        List<QueryLiteral> result = new ArrayList<QueryLiteral>(issues.size());
+        for (Issue issue : issues) {
+            result.add(new QueryLiteral(operand, issue.getKey()));
+        }
+        return result;
+    }
+
+    private SharedEntitySearchParameters prepareSharedEntirySearchParameyer(String filter) {
+        return new SharedEntitySearchParametersBuilder().setName(filter).toSearchParameters();
+
+    }
+
+    private SearchRequest tryToFetchById(User user, String filter) {
+        if (!filter.matches("\\d+"))
+            return null;
+        return searchRequestService.getFilter(new JiraServiceContextImpl(user), Long.valueOf(filter));
+    }
+
+    private static class SearchRequestByNameConsumer implements Consumer<SearchRequest> {
+
+        private String name;
+        private SearchRequest found = null;
+
+        public SearchRequestByNameConsumer(String name) {
+            this.name = name;
+        }
+
+        public void consume(SearchRequest t) {
+            if (t.getName().equals(name)) {
+                found = t;
+            }
+        }
+
+        public SearchRequest getFound() {
+            return found;
+        }
+    }
 }
